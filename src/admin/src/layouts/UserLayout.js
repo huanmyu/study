d3d6c106eedd2e5361c1ba77207f6b4e2cb77b01
Bowen Chen <bowenchen886@gmail.com>
2017-11-30 22:27:47 +0800

    add admin for study

---
+++ src/admin/src/layouts/UserLayout.js
@@ -0,0 +1,75 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Link, Route } from 'dva/router';
+import DocumentTitle from 'react-document-title';
+import { Icon } from 'antd';
+import GlobalFooter from '../components/GlobalFooter';
+import styles from './UserLayout.less';
+
+const links = [{
+  title: '帮助',
+  href: '',
+}, {
+  title: '隐私',
+  href: '',
+}, {
+  title: '条款',
+  href: '',
+}];
+
+const copyright = <div>Copyright <Icon type="copyright" /> 2017 Continuous Study Platform</div>;
+
+class UserLayout extends React.PureComponent {
+  static childContextTypes = {
+    location: PropTypes.object,
+  }
+  getChildContext() {
+    const { location } = this.props;
+    return { location };
+  }
+  getPageTitle() {
+    const { getRouteData, location } = this.props;
+    const { pathname } = location;
+    let title = 'Continuous Study Platform';
+    getRouteData('UserLayout').forEach((item) => {
+      if (item.path === pathname) {
+        title = `${item.name} - Continuous Study Platform`;
+      }
+    });
+    return title;
+  }
+  render() {
+    const { getRouteData } = this.props;
+
+    return (
+      <DocumentTitle title={this.getPageTitle()}>
+        <div className={styles.container}>
+          <div className={styles.top}>
+            <div className={styles.header}>
+              <Link to="/">
+                <img alt="" className={styles.logo} src="https://gw.alipayobjects.com/zos/rmsportal/NGCCBOENpgTXpBWUIPnI.svg" />
+                <span className={styles.title}>Continuous Learning</span>
+              </Link>
+            </div>
+            <div className={styles.desc}>Continuous Study Platform 是一个持续坚持的终身学习平台</div>
+          </div>
+          {
+            getRouteData('UserLayout').map(item =>
+              (
+                <Route
+                  exact={item.exact}
+                  key={item.path}
+                  path={item.path}
+                  component={item.component}
+                />
+              )
+            )
+          }
+          <GlobalFooter className={styles.footer} links={links} copyright={copyright} />
+        </div>
+      </DocumentTitle>
+    );
+  }
+}
+
+export default UserLayout;
---
+++ src/main/resources/app/src/components/UserActive.vue
@@ -0,0 +1,95 @@
+<template>
+  <div class="user-active-page">
+    <el-form :model="ruleForm2" status-icon :rules="rules2" ref="ruleForm2" label-width="100px" class="active-ruleForm">
+      <el-form-item label="昵称" prop="username">
+        <el-input v-model="ruleForm2.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码" prop="pass">
+        <el-input type="password" v-model="ruleForm2.pass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item label="确认密码" prop="checkPass">
+        <el-input type="password" v-model="ruleForm2.checkPass" auto-complete="off"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="submitForm('ruleForm2')">提交</el-button>
+        <el-button @click="resetForm('ruleForm2')">重置</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'active',
+  data () {
+    var validateName = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入昵称'))
+      } else {
+        callback()
+      }
+    }
+    var validatePass = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请输入密码'))
+      } else {
+        if (this.ruleForm2.checkPass !== '') {
+          this.$refs.ruleForm2.validateField('checkPass')
+        }
+        callback()
+      }
+    }
+    var validatePass2 = (rule, value, callback) => {
+      if (value === '') {
+        callback(new Error('请再次输入密码'))
+      } else if (value !== this.ruleForm2.pass) {
+        callback(new Error('两次输入密码不一致!'))
+      } else {
+        callback()
+      }
+    }
+    return {
+      ruleForm2: {
+        username: '',
+        pass: '',
+        checkPass: ''
+      },
+      rules2: {
+        pass: [
+          { validator: validatePass, trigger: 'blur' }
+        ],
+        checkPass: [
+          { validator: validatePass2, trigger: 'blur' }
+        ],
+        username: [
+          { validator: validateName, trigger: 'blur' }
+        ]
+      }
+    }
+  },
+  methods: {
+    submitForm (formName) {
+      this.$refs[formName].validate((valid) => {
+        if (valid) {
+          alert('submit!')
+        } else {
+          console.log('error submit!!')
+          return false
+        }
+      })
+    },
+    resetForm (formName) {
+      this.$refs[formName].resetFields()
+    }
+  }
+}
+</script>
+
+  <!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.user-active-page {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
